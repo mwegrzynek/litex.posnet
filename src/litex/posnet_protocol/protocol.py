497055c9bbb5794e5368f43a1a3c39ae30907bd7
Michał Węgrzynek <mwegrzynek@litex.pl>
2016-09-22 12:14:04 +0200

    Działający framing zgodnie z protokołem

--- src/litex/posnet_protocol/protocol.py
+++ src/litex/posnet_protocol/protocol.py
@@ -1,32 +1,40 @@
 # -*- coding: UTF-8 -*-
 from functools import partial
-from construct import Struct, Byte, Magic, String, CString, \
-    OptionalGreedyRange, Switch, Pointer, Anchor, Sequence, Value
+from binascii import hexlify
+from construct import Struct, Byte, Bytes, Const, String, CString, \
+    Switch, Pointer, Anchor, Sequence, GreedyRange, Computed, this, \
+    RawCopy, Checksum
 
-from PyCRC.CRC16 import CRC16
+from PyCRC.CRCCCITT import CRCCCITT
 
-TabTerminated = partial(CString, terminators=b'\x09')
+crc16 = lambda val: '{:04x}'.format(CRCCCITT().calculate(val)).upper()
 
-PosnetParameter = Struct('parameter',
-    String('name', 2),
-    TabTerminated('value')
+TabTerminated = CString(terminators=b'\x09')
+
+PosnetParameter = Struct(
+    'name' / String(length=2),
+    'value' / TabTerminated
 )
 
-PosnetToken = Struct('token',
-    Magic('@'),
-    String('value', 4)
+PosnetToken = Struct(
+    Const('@'),
+    'value' / String(length=4)
 )
 
-PosnetFrame = Struct('frame',
-    Magic(b'\x02\x09'),
-    TabTerminated('instruction'),
-    Switch('parameters', lambda ctx: ctx.instruction, {
-            'rtcset': Sequence('params', PosnetParameter)
-        },
-        default=OptionalGreedyRange(PosnetParameter),
-    ),
-    Magic('#'),
-    Value('crc16', lambda ctx: ctx.instruction + ctx.parameters)
-
-    #CRC16().calculate()
+PosnetFrame = Struct(
+     Const(b'\x02'),
+     'summed' / RawCopy(
+         Struct(
+             'instruction' / TabTerminated,
+             'parameters' /
+                Switch(this.instruction, dict(
+                     rtcset=Sequence(PosnetParameter)
+                ),
+                default=GreedyRange(PosnetParameter)
+              ),
+         )
+      ),
+      Const('#'),
+      'crc' / Checksum(Bytes(4), crc16, 'summed'),
+      Const('\x03')
 )
--- src/litex/posnet_protocol/tests/test_framing.py
+++ src/litex/posnet_protocol/tests/test_framing.py
@@ -1,16 +1,38 @@
 # -*- coding: UTF-8 -*-
 from nose.tools import eq_
 
+def test_crc():
+    from ..protocol import crc16
+    eq_(crc16('ERR\t?5\t'), '7F84')
+
+def test_single_parameter():
+    from ..protocol import PosnetParameter
+
+    frame = PosnetParameter.build(dict(name='da', value='20160101'))
+    eq_(frame, 'da20160101\t')
+
+def test_multiple_parameters():
+    from construct import GreedyRange
+    from ..protocol import PosnetParameter
+
+    frame = GreedyRange(PosnetParameter).build([
+        dict(name='da', value='20160101'),
+        dict(name='vat', value='23')
+    ])
+    eq_(frame, 'da20160101\tva23\t')
+
 def test_rtcget():
     from construct import Container
     from ..protocol import PosnetFrame
 
     get_clock_instr = Container(
-        instruction='rtcset',
-        parameters=[
-            Container(name='da', value='20160101')
-        ]
+        summed=dict(value=dict(
+            instruction='rtcset',
+            parameters=[
+                Container(name='da', value='2016-09-22,12:05')
+            ]
+        ))
     )
 
     frame = PosnetFrame.build(get_clock_instr)
-    eq_(frame, 'ble')
+    eq_(frame, '\x02rtcset\tda2016-09-22,12:05\t#8534\x03')
